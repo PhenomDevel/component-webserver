96a26773ccd596983785da6260cb50666bd21a5e
Kevin Kaiser <kkaiser@doctronic.de>
2020-08-20 12:33:16 +0200

    Release version 1.0.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject de.phenomdevel/component-webserver "1.0.0-SNAPSHOT"
+(defproject de.phenomdevel/component-webserver "1.0.0"
   :dependencies
   [[org.clojure/clojure "1.10.1"]
    [org.clojure/core.async "1.2.603"]
